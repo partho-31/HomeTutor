8752ab3944a9dd3961c892b4faf47e67a65933e5
Partho <parthoemperors@gmail.com>
2025-03-28 03:10:37 +0600

    README file add & some bugs fixed

--- api/permissions.py
+++ api/permissions.py
@@ -8,6 +8,13 @@ class IsAdminOrTeacherOrReadOnly(permissions.BasePermission):
         if request.user.is_authenticated and request.user.role == 'Teacher':
             return True
         return bool(request.user.is_staff)
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user.is_authenticated and request.user.is_staff:
+            return True
+        return obj.user == request.user
     
 
 
